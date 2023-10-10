9174c0384fd4f0cdb271341c184c8602597110d4
Anton Ozerov <lamerork@gmail.com>
2023-10-10 21:02:03 +0300

    Code optimization

--- bot.py
+++ bot.py
@@ -5,12 +5,6 @@ import telegram
 from time import sleep
 
 
-def send_text_telegram(token, chat_id, text):
-
-    bot = telegram.Bot(token=token)
-    bot.send_message(chat_id=chat_id, text=text, timeout=300, parse_mode=telegram.ParseMode.HTML)
-
-
 def main():
     env = Env()
     env.read_env()
@@ -22,40 +16,40 @@ def main():
         "Authorization": env.str('DVMN_TOKEN')
     }
 
-    params = {
-            "timestamp": '1695561487'
-        }
+    params = {}
 
-    token = env.str('TELEGRAM_TOKEN')
-    chat_id = env.str('CHAT_ID')
+    bot = telegram.Bot(token=env.str('TELEGRAM_TOKEN'))
+
+    chat_id = env.str('TELEGRAM_CHAT_ID')
 
     while True:
 
         try:
             response = requests.get(url, headers=headers, params=params)
             response.raise_for_status()
-            response_dict = response.json()
-            if response_dict['status'] == 'timeout':
-                logging.info(f'Нет новых сообщений: {response_dict["timestamp_to_request"]}')
+            dvmn_response = response.json()
+
+            if dvmn_response['status'] == 'timeout':
+                logging.info(f'Нет новых сообщений: {dvmn_response["timestamp_to_request"]}')
+
             else:
-
                 logging.info('Сообщение получено')
-                params['timestamp'] = response_dict['last_attempt_timestamp']
+                params['timestamp'] = dvmn_response['last_attempt_timestamp']
 
-                for new_attempt in response_dict['new_attempts']:
+                for new_attempt in dvmn_response['new_attempts']:
                     text = f'Проверка работы <b>{new_attempt["lesson_title"]}</b> завершена'
                     if new_attempt['is_negative']:
                         text += '<i>\n\nПреподователю всем понравилось!</i>'
                     else:
                         text += '<i>\n\nВ работе нашлись ошибки!!!</i>'
-                    send_text_telegram(token, chat_id, text)
-                    sleep(1)
+                    bot.send_message(chat_id=chat_id, text=text, timeout=300, parse_mode=telegram.ParseMode.HTML)
 
         except requests.exceptions.ReadTimeout:
             logging.info('Время соединения истекло')
 
         except ConnectionError:
             logging.info('Ошибка соединения')
+            sleep(60)
 
 
 if __name__ == '__main__':
